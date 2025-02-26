a2bf4c44d8d4e886502ceb45cf5c84f498521eca
rrarrudadev <rrarrudaacad@gmail.com>
2025-02-26 18:08:19 -0300

    Update Task Feature

--- main.py
+++ main.py
@@ -2,23 +2,36 @@ import os
 import json
 from datetime import datetime
 
-#Function to format the task description string: removing double quotes and useless empty spaces)
+#Function to format the task description string: removing double quotes and useless empty spaces
 def strFormat(task_string):
         new = task_string.replace('"',' ').strip().split()
         new = " ".join(str(x) for x in new)
         return new
 
-#Function to add a task
-def add_task(desc):
-        desc_clean = strFormat(desc)
+#Function to check if the file exists and return the correctly position to add a new task
+def lines_check():
+        count = 0
+        try:
+                f = open("tasks.json", "r")
+                for x in f:
+                        count+=1
+                f.close()
+        except:
+                pass
+        return count
 
+#Function to add a new task at the bottom of the file
+def add_task(desc):
+        desc = strFormat(desc)
         time = datetime.now().replace(microsecond=0).strftime("%Y-%m-%d %H:%M:%S")
-        tasks_dict["id"] = len(tasks_list)+1
-        tasks_dict["description"] = desc_clean
+
+        count = lines_check()
+
+        tasks_dict["id"] = count+1
+        tasks_dict["description"] = desc
         tasks_dict["status"] = "Not Done"
         tasks_dict["createdAt"] = time
         tasks_dict["updatedAt"] = time
-        tasks_list.append(tasks_dict.copy())
 
         json_task = json.dumps(tasks_dict.copy())
         f = open("tasks.json", "a")
@@ -28,6 +41,35 @@ def add_task(desc):
 
         x = tasks_dict.get("id")
         print(f"Task added successfully (ID: {x})")
+
+#Function to update a task by the task id.
+def update_task(task_id, new_desc):
+        new_desc = strFormat(new_desc)
+        time = datetime.now().replace(microsecond=0).strftime("%Y-%m-%d %H:%M:%S")
+
+
+        f = open("tasks.json", "r")
+        for x in f:
+                tasks_list.append(json.loads(x))
+        f.close()
+
+        for x in tasks_list:
+                if x["id"] == task_id:
+                        x.update({"description":new_desc})
+                        x.update({"updatedAt":time})
+                        tasks_list[task_id-1] = x
+                        break
+
+        f = open("tasks.json", "w")
+        for x in tasks_list:
+                task = json.dumps(x)
+                f.write(task)
+                f.write("\n")
+        f.close()
+
+        tasks_list.clear()
+
+        print(f"Task updated sucessfully (ID: {task_id})")
 
 tasks_dict = dict()
 tasks_list = list()
@@ -49,10 +91,32 @@ while(True):
                 if args[1].startswith('"') and args[1].endswith('"'):
                         task = args[1]
                         add_task(task)
-
-                        args.clear()
                 else:
-                        print("Incorrect task input. You must type the task between double quotes.")
+                        print('Incorrect add input. Try: COMMAND "TASK_DESCRIPTION"')
+
+        elif entrada.startswith("update "):
+                args = entrada.split(" ", 2)
+                command = args[0]
+                if int(args[1]) and args[2].startswith('"') and args[2].endswith('"'):    
+                        task_id = args[1]
+                        new_desc = args[2]
+                        task_id = int(task_id)
+                        count = lines_check()
+                        if task_id > count:
+                                print(f"Task {task_id} doesn't exist")
+                        
+                        else:
+                                update_task(task_id, new_desc)
+                else:
+                        print('Incorrect update command input. Try: COMMAND TASK_ID "TASK_DESCRIPTION".')
+        elif entrada == "exit":
+                break
         else:
-                pass
-                
+                print("Incorrect Input")
+
+        args.clear()
+
+if os.path.exists("tasks.json"):
+        os.remove("tasks.json")
+else:
+        print("The file does not exist")
