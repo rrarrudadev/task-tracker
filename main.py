c529b14925a8c3904bbfd9772a5c5c22b343b6c8
rrarrudadev <rrarrudaacad@gmail.com>
2025-03-18 21:38:59 -0300

    errors and edge cases
     handling errors and edge cases;

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 import json
 from datetime import datetime
 
-#Function to format the task description string: removing double quotes and useless empty spaces
+#Function to clear the task description, removing double quotes and white espaces
 def strFormat(task_string):
 
         new = task_string.replace('"',' ').strip().split()
         new = " ".join(str(x) for x in new)
         return new
 
-#Function to check if the file exists and return the correctly position to add a new task
+#Function to check if the file exists and return the number of tasks inside the file
 def lines_check():
 
         count = 0
@@ -103,6 +103,7 @@ def delete_task(task_id):
         tasks_list.clear()
         print(f"Task {task_id} deleted")
 
+#Function to set the task status to In-Progress using the ID as a reference
 def mark_in_progress(task_id):
 
         f = open("tasks.json", "r")
@@ -125,6 +126,7 @@ def mark_in_progress(task_id):
 
         tasks_list.clear()
 
+#Function to set the task status to Done using the ID as a reference
 def mark_done(task_id):
 
         f = open("tasks.json", "r")
@@ -146,21 +148,27 @@ def mark_done(task_id):
         f.close()
 
         tasks_list.clear()
-        
+
+#Function to list all tasks created     
 def listall_tasks():
-        f = open("tasks.json","r")
-        for x in f:
-                tasks_list.append(json.loads(x))
-        f.close()
+        try:
+                f = open("tasks.json","r")
+                for x in f:
+                        tasks_list.append(json.loads(x))
+                f.close()
+
+                print("ID    Status      Task")
+
+                for x in tasks_list:
+                        print(f"|{x['id']}| { x['status']} | {x['task']}")
+        except:
+                print("No tasks created.")
         
-        print("ID    Status      Task")
-
-        for x in tasks_list:
-                print(f"|{x['id']}| { x['status']} | {x['task']}")
-
         tasks_list.clear()
 
+#Function to list all tasks that are done
 def list_done():
+
         f = open("tasks.json","r")
         for x in f:
                 tasks_list.append(json.loads(x))
@@ -174,6 +182,7 @@ def list_done():
 
         tasks_list.clear()
 
+#Function to list all tasks that are no done or initiated
 def list_todo():
         f = open("tasks.json","r")
         for x in f:
@@ -188,6 +197,7 @@ def list_todo():
 
         tasks_list.clear()
 
+#Function to list all tasks that are in progress
 def list_inprogress():
         f = open("tasks.json","r")
         for x in f:
@@ -207,16 +217,14 @@ tasks_dict = dict()
 tasks_list = list()
 args = list()
 
-
 while(True):
-        entrada = input("task-cli ")
+        entrada = input("\033[1;32;40m task-cli \033[1;33;40m")
         
-        # Clear command to clear the CLI #
+        # Command to clear the CLI
         if entrada == "cls":
-                os.system('cls')
-        
+                os.system('cls') 
         # ADD #
-        if entrada.startswith("add "):
+        elif entrada.startswith("add "):
                 args = entrada.split(" ", 1)
                 command = args[0]
 
@@ -224,28 +232,31 @@ while(True):
                         task = args[1]
                         add_task(task)
                 else:
-                        print('Incorrect add input. Try: COMMAND "TASK_DESCRIPTION"')
+                        print("\033[1;31;40m Incorrect Input")
         # UPDATE #
         elif entrada.startswith("update "):
                 args = entrada.split(" ", 2)
                 command = args[0]
-                if int(args[1]) and args[2].startswith('"') and args[2].endswith('"'):    
-                        task_id = args[1]
-                        new_task = args[2]
-                        task_id = int(task_id)
-                        count = lines_check()
-                        if task_id > count:
-                                print(f"Task {task_id} doesn't exist")
-                        
+                try:
+                        if int(args[1]) and args[2].startswith('"') and args[2].endswith('"'):
+                                task_id = args[1]
+                                new_task = args[2]
+                                task_id = int(task_id)
+                                count = lines_check()
+                                if task_id > count:
+                                        print(f"Task {task_id} doesn't exist")
+                                
+                                else:
+                                        update_task(task_id, new_task)
                         else:
-                                update_task(task_id, new_task)
-                else:
-                        print('Incorrect update command input. Try: COMMAND TASK_ID "TASK_DESCRIPTION".')
+                                print("\033[1;31;40m Incorrect Input")
+                except:
+                        print("\033[1;31;40m Incorrect Input")
         # DELETE #
         elif entrada.startswith("delete "):
                 args = entrada.split(" ", 1)
                 command = args[0]
-                if int(args[1]):
+                try:
                         task_id = args[1]
                         task_id = int(task_id)
                         count = lines_check()
@@ -253,11 +264,14 @@ while(True):
                                 print(f"Task {task_id} doesn't exist")
                         else:
                                 delete_task(task_id)
+                except:
+                        print("\033[1;31;40m Incorrect Input")
+
         # MARK IN PROGRESS #
         elif entrada.startswith("mark-in-progress "):
                 args = entrada.split(" ", 1)
                 command = args[0]
-                if int(args[1]):
+                try:
                         task_id = args[1]
                         task_id = int(task_id)
                         count = lines_check()
@@ -265,11 +279,13 @@ while(True):
                                 print(f"Task {task_id} doesn't exist")
                         else:
                                 mark_in_progress(task_id)
-        # MARK DONE #                                
+                except:
+                        print("\033[1;31;40m Incorrect Input")
+        # MARK DONE #
         elif entrada.startswith("mark-done "):
                 args = entrada.split(" ", 1)
                 command = args[0]
-                if int(args[1]):
+                try:
                         task_id = args[1]
                         task_id = int(task_id)
                         count = lines_check()
@@ -277,6 +293,8 @@ while(True):
                                 print(f"Task {task_id} doesn't exist")
                         else:
                                 mark_done(task_id)
+                except:
+                        print("\033[1;31;40m Incorrect Input")
 
         # LISTING ALL TASKS#
         elif entrada == "list":
@@ -286,20 +304,25 @@ while(True):
         elif entrada.startswith("list "):
                 args = entrada.split(" ", 1)
                 command = args[0]
-                if args[1] == "done":
-                        list_done()
-                elif args[1] == "todo":
-                        list_todo()
-                elif args[1] == "in-progress":
-                        list_inprogress()
+                count = lines_check()
+                if count < 1:
+                        print(f"No tasks created.")
+                else:
+                        if args[1] == "done":
+                                list_done()
+                        elif args[1] == "todo":
+                                list_todo()
+                        elif args[1] == "in-progress":
+                                list_inprogress()
+                        else:
+                                print("\033[1;31;40m Incorrect Input")
         elif entrada == "exit":
+                if os.path.exists("tasks.json"):
+                        os.remove("tasks.json")
+                else:
+                        print("The file does not exist.")
                 break
         else:
-                print("Incorrect Input")
+                print("\033[1;31;40m Incorrect Input")
 
         args.clear()
-
-if os.path.exists("tasks.json"):
-        os.remove("tasks.json")
-else:
-        print("The file does not exist")
